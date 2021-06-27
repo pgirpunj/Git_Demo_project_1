d501fb8ff6071810011999c4b95a2c579f831df7
HP <HP@192.168.0.4>
2021-06-27 21:43:48 +0530

    This Is My First Commit.

---
+++ src/demo_1.java
@@ -0,0 +1,19 @@
+
+public class demo_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	int size =10;
+	for(int i=1;i<=size;i++) {
+		for (int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+		
+	}
+
+}
