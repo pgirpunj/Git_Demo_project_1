297b4ac85478c90e9c9fad3ba8ba1106b5c50f65
pgirpunj <86566176+pgirpunj@users.noreply.github.com>
2021-07-14 10:10:44 +0530

    Update myfirstclass.java

--- src/mypack/myfirstclass.java
+++ src/mypack/myfirstclass.java
@@ -4,7 +4,8 @@ public class myfirstclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello");
+System.out.println("hi");
+		//changed hello to hi
 	}
 
 }
